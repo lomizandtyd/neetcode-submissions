a6f89da918146a1ab6b9ae45d12b1aa9a7e346f1
lizard9208 <lomizandtyd@gmail.com>
2026-08-18 13:00:19 -0700

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        mins, minl = "", 1e10
+
+        l = 0
+
+        tmap = Counter(t)
+        cmap = tmap.copy()
+
+        def findall():
+            nonlocal cmap
+            for v in cmap.values():
+                if v > 0:
+                    return False
+            return True
+
+        while l < len(s) and s[l] not in tmap:
+            l += 1
+
+        r = l
+        while l < len(s):
+            # find a match
+            while r < len(s) and not findall():
+                if s[r] in cmap:
+                    cmap[s[r]] -= 1
+                r += 1
+
+            #
+            if (r-l) < minl and findall():
+                minl = r - l
+                mins = s[l:r]
+
+            # print(l, r, mins)
+            # advance
+            if l < len(s) and s[l] in cmap:
+                cmap[s[l]] += 1
+            l += 1
+
+            # skip 
+            while l < r and (s[l] not in cmap or cmap[s[l]] < 0):
+                if s[l] in cmap:
+                    cmap[s[l]] += 1
+                l += 1
+            # print("- ", l, r, s[l:r], s[l] in cmap if l < len(s) else -1)
+
+        return mins
